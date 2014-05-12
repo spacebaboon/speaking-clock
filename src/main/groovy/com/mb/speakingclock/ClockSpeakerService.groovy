cb3a6d587c622cb7d4f135889c4bec28f9c29430
Ben Avery <spacebaboon@gmail.com>
2014-05-13 00:15:37 +0100

    refactor to clean up code

--- src/main/groovy/com/mb/speakingclock/ClockSpeakerService.groovy
+++ src/main/groovy/com/mb/speakingclock/ClockSpeakerService.groovy
@@ -2,35 +2,34 @@ package com.mb.speakingclock
 
 class ClockSpeakerService {
 
-    private def numbersToTwentyInEnglish = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    private def numbersInEnglish = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
                     'eleven' ,'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-    private def tenToFiftyInTensInEnglish = ['ten', 'twenty', 'thirty', 'forty', 'fifty']
-    private def INDEX_OFFSET = 1
+    private def multiplesOfTenInEnglish = ['zero', 'ten', 'twenty', 'thirty', 'forty', 'fifty']
 
 
     String speak(String digital) {
-        String hour = speakHour(digital)
-        String minute = speakMinute(digital)
+        String hours = speakHours(digital)
+        String minutes = speakMinutes(digital)
         String phase = speakPhaseOfDay(digital)
 
-        return "$hour $minute in the $phase"
+        return "$hours $minutes in the $phase"
     }
 
 
-    private def speakHour(String digital) {
+    private def speakHours(String digital) {
         def hour = parseHour(digital)
         def hour12h = calculate12HourClockFormat(hour)
 
-        return numbersToTwentyInEnglish.get(hour12h - INDEX_OFFSET)
+        return numbersInEnglish.get(hour12h)
     }
 
-    private def speakMinute(String digital) {
+    private def speakMinutes(String digital) {
         def minutes = parseMinutes(digital)
         switch (minutes) {
             case 0:
                 return  "o'clock"
             case 1..19:
-                return numbersToTwentyInEnglish.get(minutes - INDEX_OFFSET)
+                return numbersInEnglish.get(minutes)
             default:
                 return composeNumber(minutes)
         }
@@ -60,21 +59,21 @@ class ClockSpeakerService {
     }
 
     private def parseHour(String digital) {
-        def hour = (digital.split(/:/)[0] as Integer)
+        def hour = digital.split(/:/)[0] as Integer
         return hour
     }
 
     private def parseMinutes(String digital) {
-        def minutes = (digital.split(/:/)[1] as Integer)
+        def minutes = digital.split(/:/)[1] as Integer
         return minutes
     }
 
     private def composeNumber(int minutes) {
         int tens = minutes / 10
-        def number = tenToFiftyInTensInEnglish.get(tens - INDEX_OFFSET)
+        def number = multiplesOfTenInEnglish.get(tens)
         int units = minutes % 10
         if (units) {
-            number += "-" + numbersToTwentyInEnglish.get(units - INDEX_OFFSET)
+            number += "-" + numbersInEnglish.get(units)
         }
         return number
     }
